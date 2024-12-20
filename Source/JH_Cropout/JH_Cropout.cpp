2ec7fe77112d7fed8a27118fc02bcf4db4321742
Junghyeon0710 <sie08357@naver.com>
2024-12-20 15:52:20 +0900

    Init

---
+++ Source/JH_Cropout/JH_Cropout.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JH_Cropout.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JH_Cropout, "JH_Cropout" );
