7e9f25ab0b3f19636729ad7ee2c8a5e1a08dbe86
Junghyeon0710 <sie08357@naver.com>
2024-12-26 16:43:59 +0900

    레벨 및 클래스 추가

---
+++ Source/JH_Cropout/Public/GameMode/JHGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "JHGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JH_CROPOUT_API UJHGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/JH_Cropout/Public/MainMenu/JHMainMenuGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JHMainMenuGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JH_CROPOUT_API AJHMainMenuGM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
